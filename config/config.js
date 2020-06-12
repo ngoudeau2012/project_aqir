85c3bcf3eaa24b051f0b7e62647ade9df131ea9c
Dan Weikart <danmweikart@gmail.com>
2020-06-11 20:38:04 -0700

    changed config.json to config.js, updated env vars.

--- config/config.js
+++ config/config.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
+module.exports = 
 {
   "development": {
-    "username": "root",
-    "password": "nJames93.",
-    "database": "aqir_db",
-    "host": "127.0.0.1",
+    "username": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_PASSWORD,
+    "database": process.env.MYSQL_DATABASE,
+    "host": process.env.MYSQL_HOST,
     "dialect": "mysql"
   },
   "test": {
