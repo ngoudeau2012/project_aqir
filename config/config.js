2662e2506a78c9a927c2b6f60bb197713e912a21
Dan Weikart <danmweikart@gmail.com>
2020-06-13 14:51:22 -0700

    changed config

--- config/config.js
+++ config/config.js
@@ -8,10 +8,10 @@ module.exports = {
     dialect: "mysql"
   },
   test: {
-    username: "root",
-    password: "nJames93.",
-    database: "aqir_db",
-    host: "127.0.0.1",
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST,
     dialect: "mysql"
   },
   production: {
