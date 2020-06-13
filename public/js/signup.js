fe0eb059b2afb2a21b243e51235a6dfe1517a4b9
Dan Weikart <danmweikart@gmail.com>
2020-06-13 10:24:16 -0700

    Fixed signuphtml forms

--- public/js/signup.js
+++ public/js/signup.js
@@ -31,43 +31,43 @@ $(document).ready(() => {
     signUpUser(
       userData.first_name,
       userData.last_name,
-      userData.username,
+      userData.user_name,
       userData.email,
       userData.password
     );
 
-      firstNameInput.val(""),
+    firstNameInput.val(""),
       lastNameInput.val(""),
       usernameInput.val(""),
       emailInput.val(""),
       passwordInput.val("");
   });
 
-  function signUpUser(first_name, last_name, username, email, password) {
-    console.log(first_name, last_name, username, email, password);
+  function signUpUser(first_name, last_name, user_name, email, password) {
+    console.log(first_name, last_name, user_name, email, password);
   }
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
-  // function signUpUser(first_name, last_name, username, email, password) {
-  //   $.post("/api/signup", {
-  //     first_name: first_name,
-  //     last_name: last_name,
-  //     username: username,
-  //     email: email,
-  //     password: password
-  //   })
-  //     .then(() => {
-  //       // window.location.replace("/members");
-  //       console.log(first_name, last_name, username, email, password);
+  function signUpUser(first_name, last_name, username, email, password) {
+    $.post("/api/signup", {
+      first_name: first_name,
+      last_name: last_name,
+      username: username,
+      email: email,
+      password: password
+    })
+      .then(() => {
+        window.location.replace("/");
+        console.log(first_name, last_name, username, email, password);
 
-  //       // If there's an error, handle it by throwing up a bootstrap alert
-  //     })
-  //     .catch(handleLoginErr);
-  // }
+        // If there's an error, handle it by throwing up a bootstrap alert
+      })
+      .catch(handleLoginErr);
+  }
 
-  // function handleLoginErr(err) {
-  //   $("#alert .msg").text(err.responseJSON);
-  //   $("#alert").fadeIn(500);
-  // }
+  function handleLoginErr(err) {
+    $("#alert .msg").text(err.responseJSON);
+    $("#alert").fadeIn(500);
+  }
 });
