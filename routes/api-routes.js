51548b1f5751a80fb5cc4b23407ad32ea1d67c94
Dan Weikart <danmweikart@gmail.com>
2020-06-18 12:52:18 -0700

    added cart page, updated api-route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -109,6 +109,10 @@ module.exports = function(app) {
       });
   });
 
+  app.get("/cart", function(req, res) {
+    res.render("cart");
+  });
+
   app.get("/login", function(req, res) {
     res.render("login");
   });
